dfec6f8cb9baf225d37596ddd88ca3199fe58cd9
George <web.gwh@gmail.com>
2020-11-08 15:54:05 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='short_lambda',
+    version='1.0.0',
+    author='George Harding',
+    author_email='web.gwh@gmail.com',
+    packages=['reactpy'],
+    url='https://github.com/worc3131/short_lambda',
+    license='LICENSE.txt',
+    description='A bad idea',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
